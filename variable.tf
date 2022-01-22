64375b56c14178bd97a469dacb7e414061f167e3
Rahul Singh <rstraining4@gmail.com>
2022-01-22 14:39:30 +0530

    tf

---
+++ variable.tf
@@ -0,0 +1,5 @@
+variable "aws_region" {
+  default     = "ap-south-1"
+  description = "aws region where our resources going to create choose"
+  #replace the region as suits for your requirement
+}
