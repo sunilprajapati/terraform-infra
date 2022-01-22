d2bb4f02537a94960052335b9cbd8f21c652640a
Rahul Singh <rstraining4@gmail.com>
2022-01-23 01:09:50 +0530

    tf

--- main.tf
+++ main.tf
@@ -35,10 +35,10 @@ user_data = <<-EOF
   tags = {
     Name = "web-instance"
   }
+  }
   output "instance_public_ip" {
   value = aws_instance.web.public_ip
 }
   output "instance_private_ip" {
   value = aws_instance.web.private_ip
-    }
 }
